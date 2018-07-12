275609dfc1f5c9459c248f531f73ae130b635934
Paul Suarez <paulsblogging.com@gmail.com>
2018-07-12 11:15:17 -0700

    fixed

--- src/AnalyzeWeatherData.java
+++ src/AnalyzeWeatherData.java
@@ -1,29 +1,46 @@
+import java.util.*;
+
 public class AnalyzeWeatherData {
+
     public static void main(String[] args) {
 
-        int[][] temps = {
+        int[][] weeklyMonthTemperatures = {
                 {66, 64, 58, 65, 71, 57, 60},
                 {57, 65, 65, 70, 72, 65, 51},
                 {55, 54, 60, 53, 59, 57, 61},
                 {65, 56, 55, 52, 55, 62, 57}
         };
 
+        System.out.println(Arrays.toString(findHighLow(weeklyMonthTemperatures)));
     }
 
-    public static int main(int[] arr) {
+    public static int[] findHighLow(int[][] aa) {
 
-        int largest = temps[0][0];
-        int smallest = temps[0][0];
-
-        for (int i = 1; i < temps.length; i++) {
-            if (temps[0][i] > largest) {
-                largest = temps[0][i];
+        HashSet<Integer> set = new HashSet<>();
+        for (int row = 0; row < aa.length; row++) {
+            for (int col = 0; col < aa[row].length; col++) {
+                set.add(aa[row][col]);
             }
         }
 
-        System.out.println(largest);
-        return largest;
+        int low = Collections.min(set);
+        int high = Collections.max(set);
+        List temps = new ArrayList<Integer>();
 
+        for (int i = low + 1; i < high - 1; i++) {
+            if (set.contains(i)) {
+                temps.add(i);
+            }
+        }
 
+        System.out.println("High: " + high);
+        System.out.println("Low: " + low);
+
+        for (int i = 0; i < temps.size(); i++) {
+            System.out.println("Never saw temperature: " + temps.get(i));
+        }
+
+        return new int[0];
     }
 }
+
--- src/ArrayOfArraysTest.java
+++ src/ArrayOfArraysTest.java
@@ -1,2 +1,50 @@
-public class ArrayOfArraysTest {
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayOfArraysTest {
+
+    @Test
+    void averageWeeklyTemperatures() {
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        String result = Arrays.toString(ArrayOfArrays.findAvg(weeklyMonthTemperatures));
+
+        String expected = Arrays.toString(new int[] {55, 54, 60, 53, 59, 57, 61});
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void averageWeeklyTemperatures2() {
+        int[][] weeklyMonthTemperatures = {
+                {10, 12, 13, 24, 15, 26, 17},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        String result = Arrays.toString(ArrayOfArrays.findAvg(weeklyMonthTemperatures));
+
+        String expected = Arrays.toString(new int[] {10, 12, 13, 24, 15, 26, 17});
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void averageWeeklyTemperaturesDoubled() {
+        int[][] weeklyMonthTemperatures = {
+                {99, 97, 84, 56, 45, 85, 23, 72, 21, 13, 68, 53, 26, 17},
+                {57, 65, 65, 70, 72, 65, 51, 47, 15, 95, 80, 82, 53, 74},
+                {55, 54, 60, 53, 59, 57, 61, 92, 83, 70, 79, 86, 97, 91},
+                {12, 29, 18, 11, 23, 21, 41, 12, 34, 25, 14, 25, 43, 19}
+        };
+        String result = Arrays.toString(ArrayOfArrays.findAvg(weeklyMonthTemperatures));
+
+        String expected = Arrays.toString(new int[] {12, 29, 18, 11, 23, 21, 41, 12, 34, 25, 14, 25, 43, 19});
+        assertEquals(expected, result);
+    }
 }
