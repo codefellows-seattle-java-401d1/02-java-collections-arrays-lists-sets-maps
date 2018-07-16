daab4f1c2e90630f17ad1b214f5ad9e65ec175e8
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-07-16 09:10:11 -0700

    fixed some bugs

--- src/day3/ArraysOfArrays.java
+++ src/day3/ArraysOfArrays.java
@@ -1,20 +1,44 @@
 package day3;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class ArraysOfArrays {
 
 
-    public static void averageWeeklyTemperatures(int[][] aa) {
 
+    public static int[] findAvg(int[][] aa) {
+
+        double[] tempArray = new double[aa.length];
 
         for (int row = 0; row < aa.length; row++) {
-            int total = 0;
+            int totalByRow = 0;
+            double averageByRow = 0;
+
             for (int col = 0; col < aa[row].length; col++) {
-                int value = aa[row][col];
-                total += value;
-                System.out.println(Arrays.toString(aa[row]));
+                totalByRow += aa[row][col];
+                averageByRow = (double) totalByRow / aa[row].length;
+            }
+            tempArray[row] += averageByRow;
+            System.out.println(Arrays.toString(aa[row]) + " -- Total: " + totalByRow + " -- Avg: " + averageByRow);
+        }
+
+        double lowestVal = tempArray[0];
+        int lowestIndex = 0;
+
+        for (int i = 1; i < tempArray.length; i++) {
+
+            if (tempArray[i] < lowestVal) {
+                lowestVal = tempArray[i];
+            }
+
+            if (tempArray[i] == lowestVal) {
+                lowestIndex = i;
+                System.out.println("Lowest index: " + lowestIndex);
             }
         }
+        System.out.println("Lowest avg: " + lowestVal);
+        System.out.println("Lowest avg arr: " + Arrays.toString(aa[lowestIndex]));
+        return aa[lowestIndex];
     }
 }
--- src/day3/CalculateAverages.java
+++ src/day3/CalculateAverages.java
@@ -1,16 +1,13 @@
 package day3;
 
 public class CalculateAverages {
-    public static void main(String[] args) {
-        int[] arr = {42, 15, 3, 6, 9, 14, 77, 8, 10, 2};
-        calculateAverage(arr);
-    }
 
-    public static void calculateAverage(int[] arr) {
+    public static double calculateAverage(int[] arr) {
         double total = 0;
         for (int i = 0; i < arr.length; i++) {
             total += (double) arr[i]/arr.length;
         }
         System.out.println(total);
+        return total;
     }
 }
