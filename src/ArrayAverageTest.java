3bdb6165205e3431f5ceb4ea5b50c3970578291e
Ahmed Osman <ahmedhosman41@gmail.com>
2018-07-18 21:39:53 -0700

    updated branch

--- src/ArrayAverageTest.java
+++ src/ArrayAverageTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ArrayAverageTest {
 
     @Test
-    void arrayAverage() {
+    void arrayAverageTest() {
 
         int[] arr = {1,2,3,4,5};
         double expected = 7.5;
